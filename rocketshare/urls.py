7bbd3024db9c72da0c279c74eebbb1773b9469a0
Bhavesh <bhavesh189kumar@gmail.com>
2021-06-03 13:12:06 +0530

    Added login and signup functionality (early phase)

--- rocketshare/urls.py
+++ rocketshare/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name = "index"),
+    path('signup',views.signup,name = "signup"),
+    path('logout',views.signout,name = "logout"),
+    path('login',views.signin,name = "login"),
 ]
--- rocketshare/views.py
+++ rocketshare/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,reverse,redirect,HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout,authenticate
+
 
 # Create your views here.
 def index(request):
     return  render(request,'rocketshare/index.html')
+
+def signup(request):
+    if request.method  == "GET":
+        return render(request,'rocketshare/signup.html')
+    else:
+        username, password1, password2 = request.POST['username'], request.POST['password1'], request.POST['password2']
+        user = User.objects.create_user(username = username, password = password1)
+        user.save()
+        login(request,user)
+        return redirect(reverse('index'))
+
+def signout(request):
+    logout(request)
+    return redirect(reverse('signup'))
+
+def signin(request):
+    if request.method  == "GET":
+        return render(request,'rocketshare/login.html')
+    else:
+        username, password= request.POST['username'], request.POST['password']
+        user = authenticate(username = username, password = password)
+        if user is not None:
+            login(request,user)
+            return redirect(reverse('index'))
+        else:
+            return HttpResponse("Invalid Credentials")

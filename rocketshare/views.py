3610e24a6da5833fd52328aaf51a439e0692f570
Bhavesh <bhavesh189kumar@gmail.com>
2021-06-05 23:03:53 +0530

    Login/signup functionality - Added some features and error messages and error handling (Halfway there)

--- rocketshare/views.py
+++ rocketshare/views.py
@@ -1,34 +1,83 @@
 from django.shortcuts import render,reverse,redirect,HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import login,logout,authenticate
-
+from django.contrib import messages
+import re
 
 # Create your views here.
+regex_email = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
+regex_username = '^[a-zA-Z0-9_.-]+$'
+
+def check_username(request,username: str):
+    if re.match(regex_username, username) is None:
+        messages.info(request,"Invalid Username")
+        return False
+    if User.objects.filter(username = username).exists():
+        messages.info(request,"Username Taken")
+        return False
+    return True
+
+def check_password(request,password1: str, password2: str):
+    if password1!=password2:
+        messages.info(request,"Passwords do not match")
+        return False
+    return True
+
+def check_email(request,email: str):
+    if re.match(regex_email, email) is None:
+        messages.info(request,"Invalid email")
+        return False
+    if User.objects.filter(email = email).exists():
+        messages.info(request,"User already exists with given email")
+        return False
+    return True
+
+def find_username(request, username):
+    if re.match(regex_username, username):
+        return username
+    elif re.match(regex_email, username):
+        if User.objects.filter(email = username).exists():
+            username = User.objects.get(email = username).username
+            return username
+        else:
+            return ""
+    else:
+        return ""
+
 def index(request):
     return  render(request,'rocketshare/index.html')
 
 def signup(request):
     if request.method  == "GET":
+        if request.user.is_authenticated:
+            return redirect(reverse('index'))
         return render(request,'rocketshare/signup.html')
     else:
-        username, password1, password2 = request.POST['username'], request.POST['password1'], request.POST['password2']
-        user = User.objects.create_user(username = username, password = password1)
-        user.save()
-        login(request,user)
-        return redirect(reverse('index'))
+        username, email, password1, password2 = request.POST['username'], request.POST['email'], request.POST['password1'], request.POST['password2']
+        if check_username(request,username) and check_email(request,email) and check_password(request,password1,password2):
+            user = User.objects.create_user(username = username, password = password1, email=email)
+            user.save()
+            login(request,user)
+            return redirect(reverse('index'))
+        else:
+            return redirect(reverse('signup'))
 
 def signout(request):
     logout(request)
-    return redirect(reverse('signup'))
+    return redirect(reverse('login'))
 
 def signin(request):
     if request.method  == "GET":
+        if request.user.is_authenticated:
+            return redirect(reverse('index'))
         return render(request,'rocketshare/login.html')
     else:
         username, password= request.POST['username'], request.POST['password']
+        username = find_username(request,username)
         user = authenticate(username = username, password = password)
         if user is not None:
             login(request,user)
             return redirect(reverse('index'))
         else:
-            return HttpResponse("Invalid Credentials")
+            messages.info(request,"Invalid Credentials")
+            return redirect(reverse('login'))
